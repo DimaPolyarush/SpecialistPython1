849c1b5747d32355c158e9b3d8b81ff9c87236b1
Dmitryi Polyarush <101463948+DimaPolyarush@users.noreply.github.com>
2022-06-24 12:18:53 +0300

    Update [hw]03_task_string.py

--- Module3/home_work/string/[hw]03_task_string.py
+++ Module3/home_work/string/[hw]03_task_string.py
@@ -1,3 +1,10 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = ("Посчитайте количество согласных букв в данной строке.")
+sgl = "цкнгшщзхъфвпрлджчсмтьб"
+text_lower = text.lower()
+count_sgl=0
+for i in text_lower:
+    if i in sgl:
+        count_sgl+=1
+print(count_sgl)
