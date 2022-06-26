3b84deb0afcc57010b6ce4b00d0ae918e2aa733e
Dmitryi Polyarush <101463948+DimaPolyarush@users.noreply.github.com>
2022-06-27 01:14:04 +0300

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -1,4 +1,22 @@
 # Дан список из n элементов, заполненный произвольными целыми числами в диапазоне от -100 до 100.
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
-# TODO: your code here
+import random
+
+numbers = []
+n = int(input("Введите количество чисел: "))
+i = 0
+
+while i <= n - 1:
+    numbers.append(random.randint(-100, 100))
+    print(numbers[i])
+    i += 1
+
+sum_p = 0
+count = 0
+while count < len(numbers):
+    if numbers[count] > 0 and numbers[count] % 2 == 0:
+        sum_p += numbers[count]
+    count += 1
+
+print("Сумма: ", sum_p)
