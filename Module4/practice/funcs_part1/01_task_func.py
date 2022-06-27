6e9ee44c3316b3d293d1aa1763b537150e465098
Dmitryi Polyarush <101463948+DimaPolyarush@users.noreply.github.com>
2022-06-27 21:28:23 +0300

    Update 01_task_func.py

--- Module4/practice/funcs_part1/01_task_func.py
+++ Module4/practice/funcs_part1/01_task_func.py
@@ -1,8 +1,13 @@
 # Напишите функцию, возвращающую наибольшее из четырех чисел
 
 def max4(n1, n2, n3, n4):
-    # TODO: your code here
-    pass
+    def max4(n1, n2, n3, n4):
+    max_number = n1
+    for i in (n1, n2, n3, n4):
+        if i > max_number:
+            max_number = i
+    return max_number
+
 
 
 # Тестируем функцию
