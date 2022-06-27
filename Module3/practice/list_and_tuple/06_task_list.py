54a7764fff7ad68df814fc9ff09c034cfeed4234
Dmitryi Polyarush <101463948+DimaPolyarush@users.noreply.github.com>
2022-06-27 20:15:02 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -4,4 +4,15 @@
 first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
-# TODO: your code here
+first_number = int(input("n1= "))
+second_number = int(input("n2= "))
+numbers = []
+if first_number > second_number:
+    first_number, second_number = second_number, first_number
+
+i = first_number
+while i < second_number:
+    if i % 3 == 0 and i != 0:
+        numbers.append(i)
+    i += 1
+print(numbers)
